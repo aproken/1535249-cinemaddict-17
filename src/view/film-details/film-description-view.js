b1e0bde55e052c5cf8321fba5f2fc91b94d7c9b8
aproken <aproken@yandex.ru>
2022-06-22 16:57:43 +0300

    fix

--- src/view/film-details/film-description-view.js
+++ src/view/film-details/film-description-view.js
@@ -45,11 +45,11 @@ const createFilmDescriptionTemplate = (film) => {
             </tr>
             <tr class="film-details__row">
               <td class="film-details__term">Writers</td>
-              <td class="film-details__cell">${writers}</td>
+              <td class="film-details__cell">${writers.join(', ')}</td>
             </tr>
             <tr class="film-details__row">
               <td class="film-details__term">Actors</td>
-              <td class="film-details__cell">${actors}</td>
+              <td class="film-details__cell">${actors.join(', ')}</td>
             </tr>
             <tr class="film-details__row">
               <td class="film-details__term">Release Date</td>
@@ -66,7 +66,7 @@ const createFilmDescriptionTemplate = (film) => {
             <tr class="film-details__row">
               <td class="film-details__term">Genres</td>
               <td class="film-details__cell">
-                <span class="film-details__genre">${genre}</span>
+                <span class="film-details__genre">${genre.join(', ')}</span>
             </tr>
           </table>
 
